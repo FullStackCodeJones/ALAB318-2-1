83e4a15c23171316dc6e21c807067d047bcd6e67
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-11-29 16:20:16 -0500

    I think I'm doing this right?

--- index.js
+++ index.js
@@ -4,8 +4,3 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.listen(3000);
-
-<a href="/about">Go to About</a>;
-
-app.get("/", (req, res) => res.render("home"));
-app.get("/about", (req, res) => res.render("about"));
