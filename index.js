433fcde6e5e236a2b6e4b0b89aea5808dafcf279
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-11-29 17:28:38 -0500

    Took about two hours but it was a fun learning experience

--- index.js
+++ index.js
@@ -7,20 +7,39 @@ app.use(morgan("dev"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+// Middleware to parse URL-encoded form data
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
 app.get("/", (req, res) => {
   res.render("home");
 });
+
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.get("/user/:id", (req.res) => {
-    res.send(`User ID: ${req.params.id}`);
+app.get("/user/:id", (req, res) => {
+  res.send(`User ID: ${req.params.id}`);
 });
 
-app.use((req, res, next) =>{
-    console.log(`${req.method} request for ${req.url}`);
-    next();
+app.use((req, res, next) => {
+  console.log(`${req.method} request for ${req.url}`);
+  next();
 });
 
-app.listen(3000);
+app.get("/download", (req, res) => {
+  res.download("./public/hoodoopictures.jpg"); // Fixed path
+});
+
+app.use(express.static("public"));
+
+app.post("/submit", (req, res) => {
+  console.log(req.body); // Logs parsed form data
+  res.send("Form Submitted! This Contract is Legally Binding🙃");
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
